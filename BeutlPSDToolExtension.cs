e46d0fec4354b3ac2b982d60cf33b7c894a4c841
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2025-03-29 17:10:37 +0900

    Setup

---
+++ BeutlPSDToolExtension.cs
@@ -0,0 +1,15 @@
+﻿using Beutl.Extensibility;
+
+namespace BeutlPSDTool;
+
+[Export]
+public class BeutlPSDToolExtension : Extension
+{
+    public override string Name => "PSDTool loader for Beutl";
+    public override string DisplayName => "PSDTool";
+
+    public override void Load()
+    {
+        base.Load();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# BeutlPSDTool
+
+The loader for zip package of PSDTool.
+
+## Project structure
+
+- `BeutlPSDToolExtension.cs` - Entry point of the extension.
